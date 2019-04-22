d5d754fa30caeaf3b7ad55bc7465326a0f7eae0d
RJ Salamanca <salamancaronito@gmail.com>
2019-04-22 11:04:35 -0400

    added progressive messages

--- src/hackerBob.py
+++ src/hackerBob.py
@@ -1,25 +1,30 @@
 guess_password = input('Guess the password: ')
-saved_password = 'nop'
+saved_password = 'cnffjbeq'
 
 def crack_password(password,rot):
     cipher = password
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     count = 0
-    
+    decoded = ''
+
     for letter in cipher:
         if letter.isalpha():
             get_index_of_letter = alphabet.index(letter)
             if get_index_of_letter < rot-1:
-                if(alphabet[get_index_of_letter+rot] != saved_password[count]):
-                    return False
+                if(alphabet[get_index_of_letter+rot] == saved_password[count]):
+                    print('Characters Matched: %s at index %d' % (letter,count+1))
+                    decoded += alphabet[get_index_of_letter+13]
+                else:
+                    decoded += '*'
             else:
-                if(alphabet[get_index_of_letter+rot-26] != saved_password[count]):
-                    return False
+                if(alphabet[get_index_of_letter+rot-26] == saved_password[count]):
+                    print('Characters Matched: %s at index %s' % (letter,count+1))
+                    decoded += alphabet[get_index_of_letter+13-26]
+                else:
+                    decoded += '*'
+        else:
+            decoded += '*'
         count += 1
-    return True
+    return 'Secret password is: %s ' % decoded
 
 print(crack_password(guess_password,13))
-
-# input str
-# convert str to rot 13
-# compare encoded and saved pwd
