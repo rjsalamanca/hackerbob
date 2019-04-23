fd1512c5a57f09f9a4dd1ff3792ae5612bf1835a
RJ Salamanca <salamancaronito@gmail.com>
2019-04-22 20:06:58 -0400

    updated hacker bob to use functions

--- src/hackerBob.py
+++ src/hackerBob.py
@@ -1,25 +1,55 @@
-guess_password = input('Guess the password: ')
 saved_password = 'cnffjbeq'
 
-def crack_password(password,rot):
-    cipher = password
+def encode(unhashed_password):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    count = 0
     decoded = ''
 
-    for letter in cipher:
+    for letter in unhashed_password:
+        if letter.isalpha():
+            get_index_of_letter = alphabet.index(letter)
+            if get_index_of_letter < 12:
+                decoded += alphabet[get_index_of_letter+13]
+            else:
+                decoded += alphabet[get_index_of_letter+13-26]
+        else:
+            decoded += letter
+    return decoded
+
+def crack_password(password_crack):
+    guess_password = input('Guess the password: ')
+    hashed_password = encode(guess_password)
+    count_index = 0
+    cracked = True
+
+    for letter in hashed_password:
+
+        if letter == password_crack[count_index]:
+            print('Character Matched: %s at position %d' % (letter,count_index+1))
+        else:
+            print('Character Failed: %s at position %d' % (letter, count_index+1))
+            cracked = False
+
+        count_index += 1
+    
+    if cracked:
+        return print('1337 H4X0R')
+    else:
+        return print('womp womp')
+
+"""     
+    for letter in hashed_password:
         if letter.isalpha() and count < len(saved_password):
             get_index_of_letter = alphabet.index(letter)
             if get_index_of_letter < rot-1:
                 if alphabet[get_index_of_letter+rot] == saved_password[count]:
-                    print('Characters Matched: %s at index %d' % (letter,count+1))
+                    print('Character Matched: %s at index %d' % (letter,count+1))
                     decoded += alphabet[get_index_of_letter+rot]
                 else:
                     print('Character Failed: %s at index %d' % (letter, count+1))
                     decoded += '*'
             else:
                 if alphabet[get_index_of_letter+rot-26] == saved_password[count]:
-                    print('Characters Matched: %s at index %s' % (letter,count+1))
+                    print('Character Matched: %s at index %s' % (letter,count+1))
                     decoded += alphabet[get_index_of_letter+rot-26]
                 else:
                     print('Character Failed: %s at index %d' % (letter, count+1))
@@ -35,9 +65,11 @@ def crack_password(password,rot):
     elif len(cipher) > len(saved_password):
         print('HINT: Try using a shorter password')
 
-    if(decoded == saved_password):
+    if decoded == saved_password:
         return '1337 H4X0R'
     else:
-        return 'Secret so far is: %s ' % decoded
+        print('Secret so far is: %s ' % decoded)
+        return 'womp womp' 
+"""
 
-print(crack_password(guess_password,13))
+crack_password(saved_password)
